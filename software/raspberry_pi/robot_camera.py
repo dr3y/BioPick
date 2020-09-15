4bd1f46188f6df4c0430faee3a2fceb7b93bfff2
Andrey Shur <andrey.shur@gmail.com>
2020-09-15 13:36:05 -0700

    proper contour rejection

--- software/raspberry_pi/robot_camera.py
+++ software/raspberry_pi/robot_camera.py
@@ -9,6 +9,7 @@ except ModuleNotFoundError:
 import pickle
 import glob
 import os
+from IPython.display import Image
 
 class robo_camera:
     def __init__(self,resolution="1920x1080",shutter_speed=2000,camera_matrix="camera_matrix.pckl"):
@@ -51,11 +52,17 @@ class robo_camera:
         bg_subtractor = cv2.createBackgroundSubtractorMOG2()
         fgmask = bg_subtractor.apply(bgimg_gray) #the first image is the background
 
-        kernel = np.ones((5,5), np.uint8) 
+        #kernel = np.ones((5,5), np.uint8) 
+        kernel = np.array([[0, 1, 1, 1, 0],
+                            [1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1],
+                            [1, 1, 1, 1, 1],
+                            [0, 1, 1, 1, 0]], dtype=np.uint8)
+
         ret,thresh = cv2.threshold(bgimg_gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU) 
         thresh = cv2.erode(thresh,kernel,iterations=1) #get rid of noise
         thresh = cv2.dilate(thresh,kernel,iterations=2) #fill up the holes
-        thresh = cv2.erode(thresh,kernel,iterations=4) #make it smaller
+        thresh = cv2.erode(thresh,kernel,iterations=10) #make it smaller
         return thresh, bg_subtractor
     def detect_needle(self,bg_image=None,thresh=None,bg_subtractor=None,needle_image=None):
         """detects a needle on top of a lit backdrop"""
@@ -69,11 +76,25 @@ class robo_camera:
         if(needle_image is None):
             needle_image = self.capture()
         gray = cv2.cvtColor(needle_image,cv2.COLOR_BGR2GRAY)
-        gray = bg_subtractor.apply(gray,learningRate=0) #learningrate 0 means another image never becomes the new background
-        gray = cv2.erode(gray,kernel,iterations=1) #erode a bit to get rid of junk
+        bgmask = bg_subtractor.apply(gray,learningRate=0) #learningrate 0 means another image never becomes the new background
+        bgmask = cv2.erode(bgmask,kernel,iterations=1) #erode a bit to get rid of junk
+        #bgmask = cv2.dilate(bgmask,kernel,iterations=1) #erode a bit to get rid of junk
 
-        ret,threshblurgray = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-        corners = cv2.goodFeaturesToTrack(threshblurgray, 1, .1, 10,mask=thresh,useHarrisDetector=True)
+        ret,binary_needlepic = cv2.threshold(bgmask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+        contours,_= cv2.findContours(binary_needlepic, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        biggest_contour = np.zeros((binary_needlepic.shape[0],binary_needlepic.shape[1],1),np.uint8)
+        newcontours = []
+        area_thresh = 100
+        for contour in contours:
+            carea = cv2.contourArea(contour)
+            if(carea > area_thresh):
+                newcontours += [contour]
+        cv2.drawContours(biggest_contour, newcontours, -1, (255, 255, 255), -1)
+        
+        result = cv2.bitwise_and(thresh,biggest_contour)
+        corners = cv2.goodFeaturesToTrack(biggest_contour, 1, .1, 10,mask=thresh,useHarrisDetector=True)
         corners = np.int0(corners)
         assert(len(corners[0])==1)
         return corners[0][0]
